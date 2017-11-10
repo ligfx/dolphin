e2ec8dc44c8ab24dcee616e3405cd41409e62e5b
Michael M <mchtly@gmail.com>
2017-11-10 13:26:56 -0800

    Revert Win32 changes to ControllerInterface

--- Source/Core/InputCommon/ControllerInterface/ControllerInterface.cpp
+++ Source/Core/InputCommon/ControllerInterface/ControllerInterface.cpp
@@ -6,8 +6,11 @@
 
 #include "InputCommon/ControllerInterface/ControllerInterface.h"
 
-#ifdef CIFACE_USE_WIN32
-#include "InputCommon/ControllerInterface/Win32/Win32.h"
+#ifdef CIFACE_USE_XINPUT
+#include "InputCommon/ControllerInterface/XInput/XInput.h"
+#endif
+#ifdef CIFACE_USE_DINPUT
+#include "InputCommon/ControllerInterface/DInput/DInput.h"
 #endif
 #ifdef CIFACE_USE_XLIB
 #include "InputCommon/ControllerInterface/Xlib/XInput2.h"
@@ -43,8 +46,11 @@ void ControllerInterface::Initialize(void* const hwnd)
 
   m_hwnd = hwnd;
 
-#ifdef CIFACE_USE_WIN32
-  ciface::Win32::Init(hwnd);
+#ifdef CIFACE_USE_DINPUT
+// nothing needed
+#endif
+#ifdef CIFACE_USE_XINPUT
+  ciface::XInput::Init();
 #endif
 #ifdef CIFACE_USE_XLIB
 // nothing needed
@@ -80,8 +86,11 @@ void ControllerInterface::RefreshDevices()
     m_devices.clear();
   }
 
-#ifdef CIFACE_USE_WIN32
-  ciface::Win32::PopulateDevices();
+#ifdef CIFACE_USE_DINPUT
+  ciface::DInput::PopulateDevices(reinterpret_cast<HWND>(m_hwnd));
+#endif
+#ifdef CIFACE_USE_XINPUT
+  ciface::XInput::PopulateDevices();
 #endif
 #ifdef CIFACE_USE_XLIB
   ciface::XInput2::PopulateDevices(m_hwnd);
@@ -127,8 +136,11 @@ void ControllerInterface::Shutdown()
     m_devices.clear();
   }
 
-#ifdef CIFACE_USE_WIN32
-  ciface::Win32::DeInit();
+#ifdef CIFACE_USE_XINPUT
+  ciface::XInput::DeInit();
+#endif
+#ifdef CIFACE_USE_DINPUT
+// nothing needed
 #endif
 #ifdef CIFACE_USE_XLIB
 // nothing needed
